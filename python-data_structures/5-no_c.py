ad086a5479b26eac30824cd9f8369f1828cdff5c
SamuelOnyangoOmondi <s.omondi@alustdent.com>
2022-07-12 01:22:56 -0700

    Task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
-    if (idx < 0):
-        return new_list
-    elif(idx >= len(my_list)):
-        return new_list
-    else:
-        new_list[idx] = element
-        return new_list
+def no_c(my_string):
+
+    convertList = list(my_string)
+    index = 0
+    editedString = ""
+
+    if (len(my_string)) == 0:
+        return my_string
+
+    new_string = convertList[:]
+
+    for elements in new_string[:]:
+        if elements == 'c' or elements == 'C':
+            del(new_string[index])
+            index -= 1
+        index += 1
+    return (editedString.join(new_string))
