ee4c8a828a12e00e794c1bb96ba73db08044183b
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2022-07-13 19:26:58 +0300

    Update 9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-
 def multiply_by_2(a_dictionary):
-    new_dic = dict(a_dictionary)
-    for key, val in new_dic.items():
-        new_dic[key] = val * 2
-    return new_disc 
+    new = dict()
+    for k, v in a_dictionary.items():
+        new[k] = v * 2
+    return new
