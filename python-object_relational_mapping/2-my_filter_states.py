6e91c56c8bdc248e66f370f0b92dba7d7b3ea6b4
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2022-08-17 12:51:40 +0300

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" Script  that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
 """
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
+"""
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
-
-    serv = MySQLdb.connect(host="localhost",  port=3306,
-                           user=sys.argv[1], password=sys.argv[2],
-                           database=sys.argv[3])
-
-    c = serv.cursor()
-    stateName = sys.argv[4]
-    c.execute(
-        "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(
-            stateName))
-    rows = c.fetchall()
-    for row in rows:
-        if row[1] == stateName:
-            print(row)
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
+    states = c.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
     c.close()
-    serv.close()
+    db.close()
