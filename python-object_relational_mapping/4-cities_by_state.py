274563efc0f2ca3cba7e5dabc27bf2e6b518cb76
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2022-08-17 12:56:35 +0300

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-""" Script that lists all cities from the database hbtn_0e_4_usa.
 """
+Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
+Usage: ./4-cities_by_state.py <mysql username> \
+                              <mysql password> \
+                              <database name>
+"""
+import sys
+import MySQLdb
+
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
-
-    serv = MySQLdb.connect(host="localhost",  port=3306,
-                           user=sys.argv[1], password=sys.argv[2],
-                           database=sys.argv[3])
-
-    c = serv.cursor()
-    c.execute("SELECT cities.id, cities.name, states.name\
-              FROM cities JOIN states ON cities.state_id = states.id\
-              ORDER BY id ASC")
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
-    c.close()
-    serv.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    [print(city) for city in c.fetchall()]
