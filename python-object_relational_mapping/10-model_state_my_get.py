3dccd6e7213a8f9071f5d450b2f2f12bd1d554a7
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2022-08-22 11:45:47 +0300

    Update 10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-""" module gets a state """
-
-
+""" script that prints the State object with the name passed as argument from
+the database hbtn_0e_6_usa
+"""
 if __name__ == "__main__":
-    from sys import argv
-    from model_state import State, Base
-    from sqlalchemy import create_engine
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    session = sessionmaker(bind=engine)
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     Base.metadata.create_all(engine)
-    st = session().query(State).filter(State.name == argv[4]).all()
-    if st:
-        for stat in st:
-            if stat.name == argv[4]:
-                print("{}".format(stat.id))
+
+    records = session.query(State).filter(State.name == sys.argv[4]).all()
+    if records:
+        for record in records:
+            print("{}".format(record.id))
     else:
         print("Not found")
-    session().close()
+    session.close()
