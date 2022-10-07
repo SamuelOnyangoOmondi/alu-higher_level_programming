42d1e9df90ed57ff50dd96447f8e03c936958129
SamuelOnyangoOmondi <s.omondi@alustudent.com>
2022-10-07 02:17:50 -0700

    task 8

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const temp = [];
+  let count = 0;
+  for (let i = (list.length - 1); i >= 0; i--) {
+    temp[i] = list[count];
+    count++;
+  }
+  return temp;
+};
+Footer
+© 2022 GitHub, Inc.
